3612f05db0262d001c41be11b1a7165809874791
Aziz Khoury <bentael@gmail.com>
2025-02-14 10:09:32 +0200

    w

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "building the demo server..."
+
+sleep 5
+
+echo "Which python3"
+echo "$(which python3)"
+echo "Which python"
+echo "$(which python)"
+
+sleep 1
+
+echo "Build complete!"
+exit 0
---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "starting the demo server..."
+echo "Which python3"
+echo "$(which python3)"
+echo "Which python"
+echo "$(which python)"
+
+python3 server.py
